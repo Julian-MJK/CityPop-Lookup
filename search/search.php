a8986c6b070130ba14bdf260cdd44a0d4e0d54d5
Julian <julian.kragset@gmail.com>
2019-05-14 18:26:56 +0200

    updates!

--- search/search.php
+++ search/search.php
@@ -8,17 +8,19 @@
 
 require "../0PHP/connection.php";
 
+$sortBy = "lastName";
 
 $sql_artist = "SELECT * FROM artist";
 $sql_album = "SELECT * FROM album";
 $sql_song = "SELECT * FROM song";
+$sql_genre = "SELECT * FROM album_genre";
+//$sql_genre = "SELECT album_id FROM album-genre WHERE genre LIKE %$query%";
 
 $query = $_GET["query"];
 
 echo "<h1> Results for <span class='fancyFont'>\"$query\"</span> </h1>";
 
 $query = strtolower($query);
-
 // SEPARATES THE QUERY INTO AN ARRAY, SPLIT AT EACH SPACE CHARACTER
 $queries = explode(' ', $query);
 $queriesN = count($queries);
@@ -27,6 +29,7 @@ $queriesN = count($queries);
 $artists = $kobling->query($sql_artist);
 $albums = $kobling->query($sql_album);
 $songs = $kobling->query($sql_song);
+$genres = $kobling->query($sql_genre);
 
 
 echo "<p class='smallText'> The database contains in total $artists->num_rows artists, $albums->num_rows albums and $songs->num_rows songs. </p> <br>";
@@ -53,9 +56,9 @@ for ($i = 0; $i < $queriesN; $i++) {
     // LOOPS THROUGH THE $artists TABLE AND CHECKS IF ANY VALUES ARE EQUAL TO THE CURRENT QUERY
     while ($row = $artists->fetch_assoc()) {
         if (
-            $queries[$i] === strtolower($row['firstName'])|| //Grouping this up with the previous check, I.E. ($queries[$i] || $query) ===, doesn't work.
+            $queries[$i] === strtolower($row['firstName']) || //Grouping this up with the previous check, I.E. ($queries[$i] || $query) ===, doesn't work.
             $queries[$i] === strtolower($row['lastName']) ||
-            $queries[$i] === strtolower($row['birthYear'])||
+            $queries[$i] === strtolower($row['birthYear']) ||
             stripos($row['firstName'], $queries[$i]) !== false ||
             stripos($row['lastName'], $queries[$i]) !== false
         ) {
@@ -66,22 +69,39 @@ for ($i = 0; $i < $queriesN; $i++) {
     // LOOPS THROUGH THE $artists TABLE AND CHECKS IF ANY VALUES ARE EQUAL TO THE CURRENT QUERY
     while ($row = $albums->fetch_assoc()) {
         if (
-            //$query === strtolower($row['title']) || //checks the entire query as the title and genre can contain multiple words.
-            //$query === strtolower($row['genre']) ||
             $queries[$i] === strtolower($row['title']) ||
-            //$queries[$i] === strtolower($row['genre']) ||
             $queries[$i] === strtolower($row['releaseYear']) ||
-            // Checks if the following include the query word in them.
             stripos($row['title'], $queries[$i]) !== false
-            //stripos($row['genre'], $queries[$i]) !== false
         ) {
-            //$result['album'][] = $row;
+            $result['album'][] = $row;
+        }
+
+        //NOTE - can (and should) be optimized greatly: Right now it searches through the ENTIRE table from the get go
+        //             instead, it may be faster to SQL query the searchwords.
+        while ($genreRow = $genres->fetch_assoc()) {
+            if (
+                $genreRow['album_id'] === $row['album_id']
+                &&
+                stripos($genreRow['genre'], $queries[$i]) !== false
+            ) {
+                $result['album'][] = $row;
+            }
         }
     }
 
     while ($row = $songs->fetch_assoc()) {
         if (
-            //$query === strtolower($row['title']) ||
+            $queries[$i] === strtolower($row['title']) ||
+            stripos($row['title'], $queries[$i]) !== false
+        ) {
+            $result['song'][] = $row;
+        }
+    }
+    // alternatively bare f.eks.: $sql_Artist = "SELECT * FROM artist WHERE (firstName LIKE '%$queries[$j]%' OR lastName LIKE '%$queries[$j]%') ORDER BY $sortBy"; kan og bruke "LIMIT $limit" og med et offset per page.
+    // Gjør det på denne metoden for høyere customizeability.
+
+    while ($row = $genres->fetch_assoc()) {
+        if (
             $queries[$i] === strtolower($row['title']) ||
             stripos($row['title'], $queries[$i]) !== false
         ) {
@@ -99,7 +119,7 @@ if (isset($result)) {
         // COUNTS HOW MANY ARTISTS ARE FOUND
         $count = count($result['artist']);
         echo '<h1> Found ' . $count . ' artist' . (($count > 1) ? 's' : '') . ' </h1>';
-        echo '<table class="searchResultTable"><tr><th>artist_id</th><th>firstName</th><th>lastName</th><th>birthYear</th></tr>';
+        echo '<table class="searchResultTable pop"><tr><th>artist_id</th><th>firstName</th><th>lastName</th><th>birthYear</th></tr>';
 
         // ADDS A ROW FOR EACH ARTIST FOUND
         for ($i = 0; $i < $count; $i++) {
@@ -107,7 +127,7 @@ if (isset($result)) {
             $artist_firstName = $result['artist'][$i]['firstName'];
             $artist_lastName = $result['artist'][$i]['lastName'];
             $artist_birthYear = $result['artist'][$i]['birthYear'];
-            echo"<tr onclick='window.location.href=\"../artist?a=$artist_id\"' class='tr'>" .
+            echo "<tr onclick='window.location.href=\"../artist?a=$artist_id\"' class='tr'>" .
                 "<td>$artist_id</td>" .
                 "<td>$artist_firstName</td>" .
                 "<td>$artist_lastName</td>" .
@@ -124,7 +144,7 @@ if (isset($result)) {
         // COUNTS HOW MANY ALBUMS ARE FOUND
         $count = count($result['album']);
         echo '<h1> Found ' . $count . ' album' . (($count > 1) ? 's' : '') . ' </h1>';
-        echo '<table><tr><th>album_id</th><th>artist_id</th><th>title</th><!--<th>genre</th>--><th>releaseYear</th></tr>';
+        echo '<table class="searchResultTable pop"><tr><th>album_id</th><th>artist_id</th><th>title</th><!--<th>genre</th>--><th>releaseYear</th></tr>';
 
         // ADDS A ROW FOR EACH ALBUM FOUND
         for ($i = 0; $i < $count; $i++) {
@@ -151,7 +171,29 @@ if (isset($result)) {
         // COUNTS HOW MANY SONGS ARE FOUND
         $count = count($result['song']);
         echo '<h1> Found ' . $count . ' song' . (($count > 1) ? 's' : '') . '</h1>';
-        echo '<table><tr><th>song_id</th><th>album_id</th><th>title</th></tr>';
+        echo '<table class="searchResultTable pop"><tr><th>song_id</th><th>album_id</th><th>title</th></tr>';
+
+        // ADDS A ROW FOR EACH SONG FOUND
+        for ($i = 0; $i < $count; $i++) {
+            $album_id = $result['song'][$i]['album_id'];
+            $song_id = $result['song'][$i]['song_id'];
+            $song_title = $result['song'][$i]['title'];
+            echo "<tr>" .
+                "<td>$album_id</td>" .
+                "<td>$song_id</td>" .
+                "<td>$song_title</td>" .
+                "</tr>";
+        }
+        echo '</table>';
+    }
+
+    // IF A GENRE IS FOUND
+    if (isset($result['genre'])) {
+
+        // COUNTS HOW MANY SONGS ARE FOUND
+        $count = count($result['song']);
+        echo '<h1> Found ' . $count . ' song' . (($count > 1) ? 's' : '') . '</h1>';
+        echo '<table class="searchResultTable pop"><tr><th>song_id</th><th>album_id</th><th>title</th></tr>';
 
         // ADDS A ROW FOR EACH SONG FOUND
         for ($i = 0; $i < $count; $i++) {
